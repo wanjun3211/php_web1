bd0c38914a00a76a3ec08b7c1d4c0f0f0a191904
Vic <123456wangtom@gmail.com>
2026-08-25 20:57:32 +1200

    work done on Tuesday

--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
